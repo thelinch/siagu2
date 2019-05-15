3f5a7b51e6f33a78d079a4b3ec2138a688556233
zfranklin <franklin.mijahuanca@unas.edu.pe>
2019-05-15 09:20:33 -0500

    actualizacion modulo-grados-titulos

--- app/Modules/GradosyTitulos/GradosyTitulos/Repository/Interfaces/alumnoGraduadoTituladoRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Modules\GradosyTitulos\Repository\interfaces;
-
-use App\Modules\globalModules\interfaces\repository\crudRepository;
-
-interface alumnoGraduadoTituladoRepositoryInterface extends crudRepository{
-
-    
-}
